f4fc6797152332d89fc8ffa1b007f0a474baa7b1
brandon armand <bwelsh8@gmail.com>
2018-02-10 09:58:06 +0000

    refactoring and optimization

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,26 +1,24 @@
 class HomeController < ApplicationController
+  before_action :authorized?, only: [:show]
+  before_action :unauthorized?, only: [:index]
   
   def index
     @url = Instagram.authorize_url(:redirect_uri => REDIRECT_URI)
-    code = params["code"]
-    @error = params["error_reason"]
     
+    code = params["code"]
     if code
       user = Instagram.get_access_token(code, :redirect_uri => REDIRECT_URI)
       session[:user_token] = user.access_token
-    end
-    
-    if session[:user_token]
       redirect_to :show
     end
     
+    @error = params["error_reason"]
+    if @error == 'user_denied'
+      @error = "You Denied the Authorization! Please Try Again"
+    end
   end
   
   def show
-    if session[:user_token] == nil
-      redirect_to '/'
-    end
-    
     client = Instagram.client(:access_token => session[:user_token])
     @user = client.user
     @media = client.user_recent_media
@@ -30,4 +28,17 @@ class HomeController < ApplicationController
     session[:user_token] = nil
     redirect_to '/'
   end
+  
+  protected
+    def authorized?
+      unless session[:user_token]
+        redirect_to '/'
+      end
+    end
+    
+    def unauthorized?
+      if session[:user_token]
+        redirect_to :show
+      end
+    end
 end
